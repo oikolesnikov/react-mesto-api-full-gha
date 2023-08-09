4923f24c1ecb46fb0be99be11c640b87c60d06a1
Oleg Kolesnikov <oikolesnikov@mail.ru>
2023-08-09 14:06:04 +0300

    acsd

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -76,7 +76,7 @@ const getCurrentUser = (req, res, next) => {
 const updateUser = (req, res, next) => {
   const { name, about } = req.body;
 
-  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
+  userSchema.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
     .then((user) => res.send(user))
     .catch((err) => {
       if (err.name === 'CastError' || err.name === 'ValidationError') {
