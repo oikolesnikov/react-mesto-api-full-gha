05d917f0dc920a306c81a6e0cfdcb8006aff0b7b
Oleg Kolesnikov <oikolesnikov@mail.ru>
2023-08-09 14:04:48 +0300

    dsca

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -76,7 +76,7 @@ const getCurrentUser = (req, res, next) => {
 const updateUser = (req, res, next) => {
   const { name, about } = req.body;
 
-  userSchema.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
     .then((user) => res.send(user))
     .catch((err) => {
       if (err.name === 'CastError' || err.name === 'ValidationError') {
